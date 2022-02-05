ab87c3176c1a88518f921685f1399795d014cb69
theshy456 <63041101+theshy456@users.noreply.github.com>
2022-02-05 19:03:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,37 +21,26 @@ var server = http.createServer(function (request, response) {
 
     console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-    if (path === '/') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`
-            <!DOCTYPE html>
-            <head>
-                <link rel="stylesheet" href="/x">
-            </head>
-            <body>
-                <h1>标题</h1>
-                <script src="/y"> </script>
-            </body>
-        `);
-        response.end()
-    } else if (path === '/x') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
-        response.end()
-    } else if (path === '/y') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-        response.write(`console.log('这是JS内容')`)
-        response.end()
-    } else {
-
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你输入的路径不存在对应的内容`)
-        response.end()
-    }
+if (path === "/") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/html;charset=utf-8");
+    response.write(`
+    <!DOCTYPE html>
+    <link rel="stylesheet" href="/style.css" >
+    <h1>你好</h1>
+    `);
+    response.end();
+  } else if (path === "/style.css") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/css;charset=utf-8");
+    response.write(`h1{color: red;}`);
+    response.end();
+  } else {
+    response.statusCode = 404;
+    response.setHeader("Content-Type", "text/html;charset=utf-8");
+    response.write(`你访问的页面不存在`);
+    response.end();
+  }
 
     /******** 代码结束，下面不要看 ************/
 })
